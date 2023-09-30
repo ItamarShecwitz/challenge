56eb8b86a2e3f754430108f83dea6a8229c09fd7
ItamarShecwitz <thewaterpaper11@gmail.com>
2023-09-30 23:23:25 +0300

    Switch remember his state
    The switch now remember his state  when app is closed, and switched the printers to print only on debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,18 +15,30 @@ class MyApp extends StatelessWidget {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getBool('switch_state') ?? false) {
+      _switchExampleStateKey.currentState?.resetSwitch(true);
+    }
+    if (kDebugMode) {
+      print("_switchExampleStateKey.currentState?.light");
+      print(_switchExampleStateKey.currentState?.light);
+    }
     // Gets this current time
     DateTime now = DateTime.now();
 
     // Gets the last day that the user has logged in.
-    final prefs = await SharedPreferences.getInstance();
     DateTime lastDay =
         DateTime.parse(prefs.getString('last_day') ?? now.toIso8601String());
+
     // Check if a day has passed
-    if (DateTime(now.year, now.month, now.day, now.minute).isBefore(
+    if (DateTime(now.year, now.month, now.day, now.minute).isAfter(
         DateTime(lastDay.year, lastDay.month, lastDay.day, lastDay.minute))) {
       // If a day has passed, reset the switch to false
-      _switchExampleStateKey.currentState?.resetSwitch();
+      _switchExampleStateKey.currentState?.resetSwitch(false);
+      if (kDebugMode) {
+        print("PASS");
+      }
     }
     // Update the last_day to the current time
     await prefs.setString('last_day', now.toIso8601String());
@@ -71,9 +84,9 @@ class TheSwitchState extends State<TheSwitch> {
   bool light = false;
   DateTime now = DateTime.now();
 
-  void resetSwitch() {
+  void resetSwitch(bool value) {
     setState(() {
-      light = false;
+      light = value;
     });
   }
 
@@ -87,7 +100,16 @@ class TheSwitchState extends State<TheSwitch> {
         final prefs = await SharedPreferences.getInstance();
         DateTime lastDay = DateTime.parse(
             prefs.getString('last_day') ?? now.toIso8601String());
-        print(lastDay);
+        if (kDebugMode) {
+          print(lastDay);
+        }
+        void saveValue() async {
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setBool('switch_state', newValue);
+        }
+
+        saveValue();
+
         // This is called when the user toggles the switch.
         setState(() {
           light = newValue;
