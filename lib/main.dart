565e03922798fc20f25d8b2b7147300079faf4e2
ItamarShecwitz <thewaterpaper11@gmail.com>
2023-09-30 19:18:39 +0300

    Initialized firebase on web ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:challenge/firebase_options.dart';
 
-final GlobalKey<_SwitchExampleState> _switchExampleStateKey =
-    GlobalKey<_SwitchExampleState>();
+final GlobalKey<TheSwitchState> _switchExampleStateKey =
+    GlobalKey<TheSwitchState>();
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Future<void> resetButton() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // Gets this current time
+    DateTime now = DateTime.now();
+
+    // Gets the last day that the user has logged in.
+    final prefs = await SharedPreferences.getInstance();
+    DateTime lastDay =
+        DateTime.parse(prefs.getString('last_day') ?? now.toIso8601String());
+    // Check if a day has passed
+    if (DateTime(now.year, now.month, now.day, now.minute).isBefore(
+        DateTime(lastDay.year, lastDay.month, lastDay.day, lastDay.minute))) {
+      // If a day has passed, reset the switch to false
+      _switchExampleStateKey.currentState?.resetSwitch();
+    }
+    // Update the last_day to the current time
+    await prefs.setString('last_day', now.toIso8601String());
+  }
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future<void> compare_dates() async {
-      // Gets this current time
-      DateTime now = DateTime.now();
-
-      // Gets the last day that the user has logged in.
-      final prefs = await SharedPreferences.getInstance();
-      DateTime lastDay =
-          DateTime.parse(prefs.getString('last_day') ?? now.toIso8601String());
-
-      // Check if a day has passed
-      if (DateTime(now.year, now.month, now.day, now.minute).isAfter(
-          DateTime(lastDay.year, lastDay.month, lastDay.day, lastDay.minute))) {
-        // If a day has passed, reset the switch to false
-        _switchExampleStateKey.currentState?.resetSwitch();
-      }
-      // Update the last_day to the current time
-      await prefs.setString('last_day', now.toIso8601String());
-    }
-
-    compare_dates();
+    resetButton();
 
     return const MaterialApp(home: RootPage());
   }
@@ -41,29 +45,29 @@ class RootPage extends StatefulWidget {
   const RootPage({super.key});
 
   @override
-  State<RootPage> createState() => _RootPageState();
+  State<RootPage> createState() => RootPageState();
 }
 
-class _RootPageState extends State<RootPage> {
+class RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Demo Project"),
       ),
-      body: Center(child: SwitchExample(key: _switchExampleStateKey)),
+      body: Center(child: TheSwitch(key: _switchExampleStateKey)),
     );
   }
 }
 
-class SwitchExample extends StatefulWidget {
-  const SwitchExample({super.key});
+class TheSwitch extends StatefulWidget {
+  const TheSwitch({super.key});
 
   @override
-  State<SwitchExample> createState() => _SwitchExampleState();
+  State<TheSwitch> createState() => TheSwitchState();
 }
 
-class _SwitchExampleState extends State<SwitchExample> {
+class TheSwitchState extends State<TheSwitch> {
   bool light = false;
   DateTime now = DateTime.now();
 
@@ -79,14 +83,14 @@ class _SwitchExampleState extends State<SwitchExample> {
       // This bool value toggles the switch.
       value: light,
       activeColor: const Color.fromARGB(255, 255, 124, 30),
-      onChanged: (bool new_value) async {
+      onChanged: (bool newValue) async {
         final prefs = await SharedPreferences.getInstance();
         DateTime lastDay = DateTime.parse(
             prefs.getString('last_day') ?? now.toIso8601String());
         print(lastDay);
         // This is called when the user toggles the switch.
         setState(() {
-          light = new_value;
+          light = newValue;
         });
       },
     );
